327403a700d517c78dc4488db12e62aaca8eda28
Isarel Getu <israelgetu101@gmail.com>
2023-01-27 20:56:07 -0500

    anchor is completed

---
+++ index.js
@@ -0,0 +1,26 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", () => {
+  console.log("Button Clicked");
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
+});
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+    <li>
+       <a href = '${myLeads[i]}' target = '_blank'>
+       ${myLeads[i]}
+      <a> 
+     </li>
+     `;
+    console.log(listItems);
+  }
+  ulEl.innerHTML = listItems;
+}
